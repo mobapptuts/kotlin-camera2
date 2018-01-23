535d76bc9e66f33976b26d47bc4a9504e94c3242
Nigel Henshaw <nigel@mobapptuts.com>
2018-01-23 20:48:28 +0900

    Single fragment downloading & displaying image using the Glide library

---
+++ app/src/main/java/com/mobapptuts/kotlinfragments/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.mobapptuts.kotlinfragments
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragment = FirstImageFragment.newInstance()
+        replaceFragment(fragment)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+}
---
+++ app/src/main/java/com/mobapptuts/kotlinfragments/MyAppGlideModule.kt
@@ -0,0 +1,10 @@
+package com.mobapptuts.kotlinfragments
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Created by nigelhenshaw on 2018/01/23.
+ */
+@GlideModule
+class MyAppGlideModule : AppGlideModule()
