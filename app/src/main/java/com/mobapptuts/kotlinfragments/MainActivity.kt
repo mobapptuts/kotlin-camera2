79e05f2d67b191e15440eb5e4b78ded546aaee62
Nigel Henshaw <nigel@mobapptuts.com>
2018-01-24 17:55:17 +0900

    Created ToolBar & integrated it to Navigation Drawer

--- app/src/main/java/com/mobapptuts/kotlinfragments/MainActivity.kt
+++ app/src/main/java/com/mobapptuts/kotlinfragments/MainActivity.kt
@@ -1,24 +1,41 @@
 package com.mobapptuts.kotlinfragments
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val drawerToogle by lazy {
+        ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         navigationView.setNavigationItemSelectedListener {
             selectDrawerItem(it)
             true
         }
+        drawerLayout.addDrawerListener(drawerToogle)
+    }
 
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        drawerToogle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        drawerToogle.onConfigurationChanged(newConfig)
     }
 
     private fun selectDrawerItem(item: MenuItem) {
@@ -38,19 +55,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        return when (item?.itemId) {
-            R.id.firstFragmentItem -> {
-                val fragment = FirstImageFragment.newInstance()
-                replaceFragment(fragment)
-                true
-            }
-            R.id.secondFragmentItem -> {
-                val fragment = SecondImageFragment.newInstance()
-                replaceFragment(fragment)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
+        return if (drawerToogle.onOptionsItemSelected(item)) true else super.onOptionsItemSelected(item)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
