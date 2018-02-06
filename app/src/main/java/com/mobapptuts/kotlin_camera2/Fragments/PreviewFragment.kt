940133f2a8cdd66df9926ef64d6617a0b8ff38e1
Nigel Henshaw <nigel@mobapptuts.com>
2018-02-06 17:49:54 +0900

    Added TextureView

--- app/src/main/java/com/mobapptuts/kotlin_camera2/Fragments/PreviewFragment.kt
+++ app/src/main/java/com/mobapptuts/kotlin_camera2/Fragments/PreviewFragment.kt
@@ -1,11 +1,15 @@
 package com.mobapptuts.kotlin_camera2.Fragments
 
+import android.graphics.SurfaceTexture
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import com.mobapptuts.kotlin_camera2.R
+import kotlinx.android.synthetic.main.fragment_preview.*
 
 /**
  * Created by nigelhenshaw on 2018/01/23.
@@ -13,8 +17,37 @@ import com.mobapptuts.kotlin_camera2.R
 class PreviewFragment : Fragment() {
 
     companion object {
-        fun newInstance() = PreviewFragment()
+        private val TAG = PreviewFragment::class.qualifiedName
+        @JvmStatic fun newInstance() = PreviewFragment()
     }
+
+    private val surfaceListener = object: TextureView.SurfaceTextureListener {
+        override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture?, width: Int, height: Int) {
+        }
+
+        override fun onSurfaceTextureUpdated(surface: SurfaceTexture?) = Unit
+
+        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture?) = true
+
+        override fun onSurfaceTextureAvailable(surface: SurfaceTexture?, width: Int, height: Int) {
+            Log.d(TAG, "textureSurface width: $width height: $height")
+            openCamera()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (previewTextureView.isAvailable)
+            openCamera()
+        else
+            previewTextureView.surfaceTextureListener = surfaceListener
+    }
+
+    private fun openCamera() {
+
+    }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_preview, container, false)
