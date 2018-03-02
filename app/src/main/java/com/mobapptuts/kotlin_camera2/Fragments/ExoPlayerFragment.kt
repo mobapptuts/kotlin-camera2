4246ed1cab393c1394b45b71a2ba49e84f3258be
Nigel Henshaw <nigel@mobapptuts.com>
2018-03-02 12:07:04 +0900

    add viewmodel to exoplayer fragment

--- app/src/main/java/com/mobapptuts/kotlin_camera2/Fragments/ExoPlayerFragment.kt
+++ app/src/main/java/com/mobapptuts/kotlin_camera2/Fragments/ExoPlayerFragment.kt
@@ -1,10 +1,13 @@
 package com.mobapptuts.kotlin_camera2.Fragments
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.mobapptuts.kotlin_camera2.CameraViewModel
 import com.mobapptuts.kotlin_camera2.R
 
 /**
@@ -12,8 +15,18 @@ import com.mobapptuts.kotlin_camera2.R
  */
 class ExoPlayerFragment : Fragment(){
 
+    private val cameraViewModel by lazy {
+        ViewModelProviders.of(activity!!).get(CameraViewModel::class.java)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_exoplayer, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        Log.d(TAG, "Video uri: ${cameraViewModel.videoUri}")
     }
 
     companion object {
